a71ff790ab6e698e2c31f994e3f7d54bc4f7ecc4
Natalya <iyunat@yandex.ru>
2022-08-11 00:21:35 +0300

    Creating Task003

---
+++ Homework001_lesson1/Task003/Program.cs
@@ -0,0 +1,14 @@
+﻿double numberA = new Random ().Next (1, 10);
+Console.WriteLine (numberA);
+double numberC = numberA/2;
+Console.WriteLine (numberC);
+double numberD = (numberC*10) %10;
+Console.WriteLine (numberD);
+if (numberD ==0)
+{
+    Console.WriteLine ("Число четное");
+}
+else
+{
+    Console.WriteLine ("Число не четное");
+}
