ebc6ed4c4b3da393517f45ae4df756f27807ae76
Natalya <iyunat@yandex.ru>
2022-08-14 23:06:48 +0300

    Creating Task13

--- Homework002_lesson2/Task10/Program.cs
+++ Homework002_lesson2/Task10/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход трёхзначное число и 
+//на выходе показывает вторую цифру этого числа.
+//456 -> 5     782 -> 8         918 -> 1
+
+int Number (int arg)
+{
+    int result = (arg/10)%10;
+    return result;
+}
+
+int [] array = {456, 782, 918};
+
+int b = Number (array [0]);
+Console.WriteLine (b);
+int c = Number (array [1]);
+Console.WriteLine (c);
+int d = Number (array [2]);
+Console.WriteLine (d);
+
+
+
+
