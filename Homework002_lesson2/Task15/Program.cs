906193d35fcef8aaa6c962c6b52bb6458bb74633
Natalya <iyunat@yandex.ru>
2022-08-15 00:37:40 +0300

    Solved task15

--- Homework002_lesson2/Task15/Program.cs
+++ Homework002_lesson2/Task15/Program.cs
@@ -1,2 +1,20 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да    7 -> да      1 -> нет
+
+
+Console.WriteLine ( "Введите число от 1 до 7");
+int DataNum = Convert.ToInt32 (Console.ReadLine() );
+
+if (DataNum ==6 || DataNum ==7)
+{
+    Console.WriteLine ( "да, выходной день");
+}
+else
+{
+    Console.WriteLine ( "нет, это не выходной день");
+}
+
+
+
+
