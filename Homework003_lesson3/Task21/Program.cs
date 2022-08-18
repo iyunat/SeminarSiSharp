95a7221279829f4eeb6c64a43e8021a14fede8f1
Natalya <iyunat@yandex.ru>
2022-08-18 14:37:20 +0300

    Creating Task21, Task23

---
+++ Homework003_lesson3/Task21/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите метод, который принимает на вход координаты двух точек и 
+// находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84  A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int PositionInput (string pos, string cur)
+{   Console.Write($"Введите координату {pos} точки {cur}: ");
+    return Convert.ToInt16(Console.ReadLine());
+}
+int ax = PositionInput ("x", "A");
+int ay = PositionInput ("y", "A");
+int az = PositionInput ("z", "A");
+int bx = PositionInput ("x", "B");
+int by = PositionInput ("y", "B");
+int bz = PositionInput ("z", "B");
+
+double Distance (double ax, double bx, double ay, double by, double az, double bz )
+{
+    return Math.Sqrt (((ax - bx)* (ax -bx)) + ((ay -by) * (ay -by)) + ((az -bz) *(az - bz)));
+}
+
+double resultDistance = Distance (ax, ay, az, bx, by, bz);
+Console.WriteLine($"Расстояние  {resultDistance}");
---
+++ Homework003_lesson3/Task23/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите метод, который принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+
+Console.Write("Введите число: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+
+void NumCub (int[] Num)
+{
+  int count = 0;
+  int length = Num.Length;
+  while (count < length)
+  {
+    Num [count] = Convert.ToInt32(count * count *count);
+    count++;
+  }
+}
+
+void PrintArry(int[] arg)
+{
+  int coun = arg.Length;
+  int index = 1;
+  while(index < coun)
+  {
+    Console.Write(arg[index]+ " ");
+    index++;
+  }
+} 
+
+int[] arry = new int[Num+1];
+NumCub (arry);
+PrintArry(arry);
