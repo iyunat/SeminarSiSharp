f1b617b1f294468b2896b79fb14e0fc52fbdc6f5
Natalya <iyunat@yandex.ru>
2022-08-21 00:23:39 +0300

    Creating Task27

---
+++ Homework004_lesson4/Task25/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Напишите метод, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+// нельзя использовать pow
+
+
+Console.WriteLine("Введите число A");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int degree = A;
+
+int NumDegree (int A, int B)
+{
+    for (int i = 1; i < B; i++)
+{
+degree = degree * A;
+}
+return degree;
+}
+
+int res = NumDegree (A, B);
+Console.WriteLine ($"A в степени B равно {res}");
---
+++ Homework004_lesson4/Task27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Hello, World!");
+Console.Write("Введите число: ");
+
+int Num = Convert.ToInt32(Console.ReadLine());
+
+int NumSum (int Num)
+{
+  int Sum = 0;
+  while (Num > 0)
+  {
+    Sum = Sum + Num % 10;
+    Num /=10;
+  }
+  return Sum;
+}
+
+int result = NumSum (Num);
+Console.WriteLine("Сумма цифр в числе: " + result);
