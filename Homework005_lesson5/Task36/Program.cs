48ba20020cf1d84cf28e5efe1b0f7f2cd0382798
Natalya <iyunat@yandex.ru>
2022-08-25 12:50:39 +0300

    Creating Task36 and Task 38

---
+++ Homework005_lesson5/Task36/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях
+
+// 1.Создаем массив:
+int [] MasCreating (int n=3)
+{
+    return new int [n];
+}
+
+// 2. Выводим массив на экран:
+ string ArrayTuString (int[] col)
+ {
+    return $"[{String.Join(' ', col)}]";
+ }
+
+//3.Метод, который заполняет массив:
+ void RandMass (int [] array)
+ {
+    int size = array.Length;
+    for (int i = 0; i< size; i++)
+    {
+        array [i]= Random.Shared.Next ();// это тоже самое , если написать array [i]= new Random().Next ()
+    }
+ }
+ 
+// 4.Находим сумму элементов, стоящих на нечётных позициях
+
+int SumNoEven(int []array)
+{
+    int size = array.Length;
+    int sumnoeven = 0;
+    for (int i = 0; i < size; i++)
+    {
+       if (i % 2 ! == 0)
+       {
+        sumnoeven = sumnoeven + array [i];
+       }   
+    }
+    return sumnoeven;
+ }
+
+int [] arr = MasCreating (3);// задаем массив. Если значение в () убрать, то создаваться массив будет n=11 (стр.5)
+ RandMass (arr);
+int values =SumNoEven (arr);
+ Console.WriteLine ($"value: {ArrayTuString (arr)}");
+ Console.WriteLine (values);
---
+++ Homework005_lesson5/Task38/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+// 1.Создаем массив:
+int [] MasCreating (int n=4)
+{
+    return new int [n];
+}
+
+// 2. Выводим массив на экран:
+ string ArrayTuString (int[] col)
+ {
+    return $"[{String.Join(' ', col)}]";
+ }
+
+ 
+ //3.Метод, который заполняет массив:
+ void RandMass (int [] array, int min=1, int max = 100)
+ {
+    int size = array.Length;
+    for (int i = 0; i< size; i++)
+    {
+        array [i]= Random.Shared.Next (min, max);// это тоже самое , если написать array [i]= new Random().Next (min, max)
+    }
+ }
+int [] arr = MasCreating (4);// задаем массив. Если значение в () убрать, то создаваться массив будет n=11 (стр.5)
+ RandMass (arr);
+ Console.WriteLine($"value: {ArrayTuString (arr)}");
+ 
+ //4. Разница между максимальным и минимальным значением
+
+    int size = arr.Length;
+    int max = arr[0];
+    int min = arr[0];
+        for (int i = 1; i <size; i++)
+        {
+        if (arr[i] < min) min = arr[i];
+        if (arr[i] > max) max = arr[i];
+        }
+        int differ = max - min;
+
+        Console.WriteLine("Разность между максимальным и минимальным элементом массива = {0}", differ);
+
+
