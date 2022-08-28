0670f3dca25d36be8f7a10c80a17f6e30022fc18
Natalya <iyunat@yandex.ru>
2022-08-28 17:07:27 +0300

    Creating Task43

--- Homework006_lesson6/Task41/Program.cs
+++ Homework006_lesson6/Task41/Program.cs
@@ -5,51 +5,51 @@
 
 
 // 1.Создаем массив:
-int [] MasCreating (int n=7)
+int[] MasCreating(int n = 7)
 {
-    return new int [n];
+    return new int[n];
 }
 
 // 2. Выводим массив на экран:
- string ArrayTuString (int[] col)
- {
+string ArrayTuString(int[] col)
+{
     return $"[{String.Join(' ', col)}]";
- }
+}
 
 
 
 //3.Метод, который заполняет массив:
- void RandMass (int [] array)
- {
+void InputMass(int[] array)
+{
     Console.Write($"Введите число М(количество чисел): ");
     int size = array.Length;
-    for (int i = 0; i< size; i++)
+    for (int i = 0; i < size; i++)
     {
-        Console.Write($"Введите {i+1} число: ");
-        array [i]= Convert.ToInt32(Console.ReadLine());
+        Console.Write($"Введите {i + 1} число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
     }
- }
+}
 
 
 //4. Метод, который считает сколько чисел пользователь ввел выше 0
 
-int QuantityNumPositiv (int [] array)
- {
+int QuantityNumPositiv(int[] array)
+{
     int count = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if (array [i] > 0)
+        if (array[i] > 0)
         {
             count++;
         }
     }
     return count;
- }
+}
 
-int [] arr = MasCreating (7);
-Console.WriteLine ($"value: {ArrayTuString (arr)}");
+int[] arr = MasCreating(7);
+Console.WriteLine($"value: {ArrayTuString(arr)}");
 
-RandMass (arr);
+InputMass(arr);
 
-int count = QuantityNumPositiv (arr);
-Console.WriteLine ($"Количество  чисел выше 0: {count}");
+int count = QuantityNumPositiv(arr);
+Console.WriteLine($"Количество  чисел выше 0: {count}");
